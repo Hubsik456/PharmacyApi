c3faa511f0a8aa1451c9d09c2712c91f41df9737
67259@student.wsiz.edu.pl <67259@student.wsiz.edu.pl>
2026-06-28 18:56:06 +0200

    Zad 3.0

--- Pharmacy/Controllers/MedicationController.cs
+++ Pharmacy/Controllers/MedicationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Pharmacy.Database;
 
 namespace Pharmacy.Controllers;
@@ -7,5 +8,30 @@ namespace Pharmacy.Controllers;
 [Route("[controller]")]
 public class MedicationController : ControllerBase
 {
+    private readonly PharmacyDbContext _context;
 
+    public MedicationController(PharmacyDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Medication>>> Get()
+    {
+        var medications = await _context.Medication.ToListAsync();
+        return Ok(medications);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Medication>> GetById(int id)
+    {
+        var medication = await _context.Medication.FindAsync(id);
+
+        if (medication == null)
+        {
+            return NotFound(new { Message = $"Lek o Id {id} nie zosta³ znaleziony." });
+        }
+
+        return Ok(medication);
+    }
 }
--- Pharmacy/Program.cs
+++ Pharmacy/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<PharmacyDbContext>(options => options.UseSqlite("Data Source=Database//Pharmacy.db"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
